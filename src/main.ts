3492e704ecd41e842b1c950a936e2edee549e0e9
Muhammad Arshad <ibnelasani@yahoo.com>
2026-01-18 18:33:35 +0500

    checking mongo conectivity

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 app.enableCors(); // <-- enable CORS
  
   //await app.listen(process.env.PORT ?? 3000,'0.0.0.0');
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+ // await app.listen(process.env.PORT || 3000, '0.0.0.0');
 console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
